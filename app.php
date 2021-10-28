7b7e7d1b4216dcf0b9d1930d1d19a0d6bebe1a61
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2021-10-28 11:24:55 -0500

    cambiando base pdf

--- app.php
+++ app.php
@@ -3,13 +3,12 @@
 require 'vendor/autoload.php';
 
 
-$pdf = Zend_Pdf::load(dirname(__FILE__)  . 'base.pdf');
+$pdf = Zend_Pdf::load(dirname(__FILE__)  .  DIRECTORY_SEPARATOR . 'base.pdf');
 $page = $pdf->pages[0];
-$page->rotate(0,0,deg2rad(270));
-$pdf->pages[0] = $page;
 $font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA);
 $page->setFont($font, 12);
-$page->drawText('Hello world!', 72, 720);
+$page->drawText('Hello world!', 150, 50);
+$pdf->pages[0] = $page;
 $pdf->save('zend2.pdf');
 
 ?>
